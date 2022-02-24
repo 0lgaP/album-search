8bb9464af329df9aaf1e9dc0e68926485bca52ae
0lgatron <perronolga@gmail.com>
2022-02-24 14:46:47 -0500

    search app

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useState } from "react";
-
+import React, { Fragment, useState, useEffect } from "react";
+import Axios from "axios";
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
 
@@ -7,6 +7,16 @@ export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
 
+  useEffect(()=>{
+    const testUrl = `https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`
+    Axios.get(testUrl)
+    .then((response)=>{
+      setResults([...response.data.results])
+      //also works (responce.data.results)
+      // [response] [[]]
+      // [response] [1,2,3,]
+    })
+  }, [term]);
   return (
     <Fragment>
       <header className="logo">
